693f8e81549a1bed58d9e0f2cdcefda9fa4946fe
anton.a <anton.adamson@gmail.com>
2024-04-23 08:51:19 +0600

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Антон Адамсон on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
