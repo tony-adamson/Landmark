bdddd1242de740f7a1c5f90d7260184283b93404
anton.a <anton.adamson@gmail.com>
2024-04-23 14:35:11 +0600

    Drawing paths and shapes

---
+++ Landmark/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Антон Адамсон on 23.04.2024.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
